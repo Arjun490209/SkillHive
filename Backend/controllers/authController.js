8b86ebe85d38b964a5602ba97ee909ef33421965
Arjun8400 <youji099@gmail.com>
2025-12-13 20:31:29 +0530

    asd

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -47,8 +47,10 @@ export const signUp = async (req, res) => {
     //! Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
+      // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
@@ -79,8 +81,10 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
+      // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
