43095466520db3f7f804c8ddc47e2e0cb64600c0
Arjun8400 <youji099@gmail.com>
2025-12-24 07:16:58 +0530

    All compete Lsm Project

--- Backend/controllers/searchController.js
+++ Backend/controllers/searchController.js
@@ -1,4 +1,7 @@
 import Course from "../models/CourseModel.js";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const searchWithAi = async (req, res) => {
   try {
@@ -7,6 +10,37 @@ export const searchWithAi = async (req, res) => {
     if (!input) {
       return res.status(400).json({ message: "Search query is required" });
     }
+
+    const ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    });
+
+    const prompt = `You are an intelligent assistant for an LMS platform. A user will type any query about what they want to learn. Your task is to understand the intent and return one **most relevant keyword** from the following list of course categories and levels:
+    - App Development
+    - AI/ML
+    - AI Tools
+    - Data Science
+    - Data Analytics
+    - Ethical Hacking
+    - UI UX Designing
+    - Web Development
+    - Other
+    - Beginner
+    - Intermediate
+    - Advance
+
+    Only reply with one single keyword form the list above that best matches the query. do not explain any thing. No extra text, 
+
+    Query : ${input}
+    `;
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    const keyword =
+      response?.candidates?.[0]?.content?.parts?.[0]?.text?.trim() || "Other";
 
     const courses = await Course.find({
       isPublished: true,
@@ -19,7 +53,23 @@ export const searchWithAi = async (req, res) => {
       ],
     });
 
-    return res.status(200).json(courses);
+    if (courses.length > 0) {
+      return res.status(200).json(courses);
+    }
+
+    const aiCourses = await Course.find({
+      isPublished: true,
+      $or: [
+        { title: { $regex: keyword, $options: "i" } },
+        { subTitle: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+        { category: { $regex: keyword, $options: "i" } },
+        { level: { $regex: keyword, $options: "i" } },
+      ],
+    });
+
+    return res.status(200).json(aiCourses);
+
   } catch (error) {
     return res.status(500).json({
       message: "Search AI error",
